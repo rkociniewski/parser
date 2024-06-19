fc9a55d1ce8782b04ac862ceb1af0c6b3673225a
rkociniewski <112274712+rkociniewski@users.noreply.github.com>
2024-06-19 10:17:55 +0200

    Update dependencies

--- src/main/kotlin/org/powermilk/Parser.kt
+++ src/main/kotlin/org/powermilk/Parser.kt
@@ -13,18 +13,17 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 import kotlin.io.path.Path
-import kotlin.io.path.exists
 import kotlin.io.path.notExists
 
 val gson: Gson = GsonBuilder()
     .registerTypeAdapter(
         LocalDateTime::class.java,
-        JsonSerializer<LocalDateTime>() { obj, _, _ ->
+        JsonSerializer<LocalDateTime> { obj, _, _ ->
             JsonPrimitive(obj.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())
         })
     .registerTypeAdapter(
         LocalDateTime::class.java,
-        JsonDeserializer() { json, _, _ -> LocalDateTime.parse(json.asJsonPrimitive.asString, formatter) })
+        JsonDeserializer { json, _, _ -> LocalDateTime.parse(json.asJsonPrimitive.asString, formatter) })
     .serializeNulls()
     .setPrettyPrinting()
     .create()
@@ -36,15 +35,17 @@ private val formatter = DateTimeFormatterBuilder().parseCaseInsensitive().append
     .toFormatter()
 
 /**
- * Function to load file to parse date and save updated content to new path. [this] is path to file with wrong date
+ * Function to load file to parse date and save updated content to a new path.
+ * Is path to file with wrong date
  * format.
  *
  * @param pathToSave path to file with updated content.
  * @exception FileNotFoundException when [this] is wrong path (File doesn't exist).
  */
 fun String.parse(pathToSave: String) {
-    if (Path(this).notExists())
+    if (Path(this).notExists()) {
         throw FileNotFoundException("File to load doesn't exist")
+    }
 
     val frames = gson.fromJson<List<Frame>>(File(this).readText(), object : TypeToken<List<Frame>>() {}.type)
     File(pathToSave).writeText(gson.toJson(frames))
